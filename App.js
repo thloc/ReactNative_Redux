3b3168915dc139fefd07070f72671be9d68c9752
Loc Tran <loc.tran@sstechvn.com>
2020-05-08 13:01:54 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styleApp.container}>
+        <View style={styleApp.header}>
+          <Text style={styleApp.appName}>EXAM 1: {'\n'}APP COMPONENT</Text>
+          <Text style={styleApp.value}>0</Text>
+        </View>
+        <View style={styleController.controller}>
+          <Text style={styleController.controllName}>CONTROLLER COMPONENT</Text>
+          <View style={styleController.buttonContainer}>
+            <TouchableOpacity style={styleController.button}>
+              <Text style={styleController.buttonText}>+</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styleController.button}>
+              <Text style={styleController.buttonText}>-</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styleApp = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'black',
+    paddingTop: 30,
+  },
+  header: {
+    flex: 1,
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+  appName: {
+    color: 'white',
+    fontSize: 30,
+    textAlign: 'center',
+  },
+  value: {
+    color: 'yellow',
+    fontSize: 40,
+  },
+});
+
+const styleController = StyleSheet.create({
+  controller: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    backgroundColor: 'yellow',
+    alignSelf: 'stretch',
+    margin: 20,
+  },
+  controllName: {
+    fontSize: 20,
+    marginBottom: 10,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  button: {
+    backgroundColor: 'black',
+    paddingHorizontal: 50,
+    paddingVertical: 25,
+    margin: 10,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 40,
+  },
+});
---
+++ README.md
@@ -0,0 +1,8 @@
+## App Demo
+
+```npx react-native init ABC
+```open -a Simulator
+```npx react-native run-ios
+
+### ERROR
+```rm -rf ios/build/; kill $(lsof -t -i:8081); react-native run-ios
