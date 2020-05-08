b1e519684c7736de8b744f1b77a8e507e2f0d4cf
Loc Tran <loc.tran@sstechvn.com>
2020-05-08 13:41:04 +0700

    Added the redux package

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+
+import Controller from './src/Controller';
 
 export default class App extends Component {
   render() {
@@ -9,17 +11,8 @@ export default class App extends Component {
           <Text style={styleApp.appName}>EXAM 1: {'\n'}APP COMPONENT</Text>
           <Text style={styleApp.value}>0</Text>
         </View>
-        <View style={styleController.controller}>
-          <Text style={styleController.controllName}>CONTROLLER COMPONENT</Text>
-          <View style={styleController.buttonContainer}>
-            <TouchableOpacity style={styleController.button}>
-              <Text style={styleController.buttonText}>+</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styleController.button}>
-              <Text style={styleController.buttonText}>-</Text>
-            </TouchableOpacity>
-          </View>
-        </View>
+
+        <Controller/>
       </View>
     );
   }
@@ -45,36 +38,6 @@ const styleApp = StyleSheet.create({
   },
   value: {
     color: 'yellow',
-    fontSize: 40,
-  },
-});
-
-const styleController = StyleSheet.create({
-  controller: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'space-around',
-    backgroundColor: 'yellow',
-    alignSelf: 'stretch',
-    margin: 20,
-  },
-  controllName: {
-    fontSize: 20,
-    marginBottom: 10,
-  },
-  buttonContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  button: {
-    backgroundColor: 'black',
-    paddingHorizontal: 50,
-    paddingVertical: 25,
-    margin: 10,
-    borderRadius: 5,
-  },
-  buttonText: {
-    color: 'white',
     fontSize: 40,
   },
 });
--- index.js
+++ index.js
@@ -2,8 +2,38 @@
  * @format
  */
 
+import React, { Component } from 'react';
 import {AppRegistry} from 'react-native';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
 import App from './App';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+
+const defaultStage = {value: 0};
+
+const reducer = (state = defaultStage, action) => {
+  if (state.type === 'UP') {
+    return { value: state.value + 1 };
+  }
+  if (state.type === 'DOWN') {
+    return { value: state.value - 1 };
+  }
+
+  return state;
+};
+
+const store = createStore(reducer);
+
+class DemoApp extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
+  }
+}
+
+AppRegistry.registerComponent(appName, () => DemoApp);
